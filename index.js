cddfedd1b60455297127179b7bde043d046d8da4
Khushi Sharma <ks8383902@gmail.com>
2025-03-30 16:26:55 +0530

    js

---
+++ index.js
@@ -0,0 +1,81 @@
+// let x = 2
+// let y = 4
+//  z = x + y
+
+//  console.log(`value of x is ${x} & value of y is ${y} & total is ${z}`)
+
+ 
+//  let cities = ["delhi","chennai","up","rajasthan","bihar"]
+//  console.log(cities)
+
+// let cities = ["delhi","chennai","up","rajasthan","bihar"]
+// for (i=0; i<cities.length ; i++){
+//     console.log(cities.length)
+// }     
+
+// $cities = ["delhi","up","rajasthan","bihar"]
+// $cities.push("haryana")let city = ["delhi","up","rajasthan","bihar"]
+// let newcity =city.concat("haryana","goa")
+// console.log(newcity)
+// $cities.push("uk","mp")
+// console.log($cities)
+
+
+// let cities =["delhi","up","rajasthan","bihar"]
+// let state = ["haryana","uk"]
+// let city =["mumbai","chennai"]
+// cities.push(...state,...city)
+// console.log(cities)
+
+
+ 
+// let city = ["delhi","up","rajasthan","bihar"]
+// city.unshift("haryana")
+// console.log(city)
+
+// let city = ["delhi","up","rajasthan","bihar"]
+// console.log(city.pop())
+
+// let city = ["delhi","up","rajasthan","bihar"]
+// console.log(city.shift())
+
+// let city = ["delhi","up","rajasthan","bihar"]
+// city.splice(2,0,"haryana","goa")
+// console.log(city)
+
+// let city = ["delhi","up","rajasthan","bihar"]
+// city.splice(3,2,"haryana")
+// console.log(city)
+
+// 
+
+// let original= {name:"arjun",score:[100,98,56]}
+// let shallowcopy ={...original}
+// shallowcopy.name="khushi"
+// shallowcopy.score[0]=60
+// console.log(original)
+// console.log(shallowcopy)
+
+// let city1 = ["delhi","up","rajasthan","bihar"]
+// let city2 =["mumbai","haryana"]
+// newcity=city1.concat(city2)
+// console.log(newcity)
+
+// let city1 = ["delhi","up","rajasthan","bihar"]
+// let city2 =["mumbai","haryana"]
+// let city3 =[...city1,...city2]
+// console.log(city3)
+
+// 
+
+// let cities =["delhi","up","rajasthan"]
+//  let output = cities.indexOf("up")
+// console.log(output)
+
+// let cities = ["delhi","up","rajasthan","up"]
+// let output = cities.indexOf("up")
+// console.log(output)
+
+// let cities ="United tech Era"
+// let output=cities.indexOf("tec")
+// console.log(output)
